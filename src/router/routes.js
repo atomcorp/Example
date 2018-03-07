dbe00008e4d2ae365a04b295a567069d0eeac784
Tom <tom.maxwell@cambridgeaudio.com>
2018-03-07 15:54:57 +0000

    sorted router, 404 pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import { Courses } from '../components/Courses/Courses.js';
 import { Course } from '../components/Course/Course.js';
 import { Module } from '../components/Module/Module.js';
 import { Assessment } from '../components/Assessment/Assessment.js';
 
-const Home = () => <div> HOME </div>;
+const NoMatch = () => <div>404</div>;
 
 const Routes = ({ resources }) => {
   return (
@@ -19,21 +19,23 @@ const Routes = ({ resources }) => {
           <li><Link to="/course/1998">Course</Link></li>
           <li><Link to="/module/2000">Module</Link></li>
           <li><Link to="/assessment/1998">Assessment</Link></li>
-          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/">Home</Link></li>
         </ul>
-        <Route path="/courses" component={() => (
-          <Courses courses={resources.courses} />
-        )} />
-        <Route path="/course/:courseId" component={route => (
-          <Course route={route} resources={resources} />
-         )} />
-        <Route path="/module/:moduleId" component={route => (
-          <Module route={route} resources={resources} />
-        )} />
-        <Route path="/assessment/:courseId" component={route => (
-          <Assessment route={route} resources={resources} />
-        )} />
-        <Route path="/home" component={Home} />
+        <Switch>
+          <Route path="/" exact component={() => (
+            <Courses courses={resources.courses} />
+          )} />
+          <Route path="/course/:courseId" component={route => (
+            <Course route={route} resources={resources} />
+          )} />
+          <Route path="/module/:moduleId" component={route => (
+            <Module route={route} resources={resources} />
+          )} />
+          <Route path="/assessment/:courseId" component={route => (
+            <Assessment route={route} resources={resources} />
+          )} />
+          <Route component={NoMatch}></Route>
+        </Switch>
       </div>
     </Router>
   )
