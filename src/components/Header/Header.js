6084b5f73900a29c05d002774dea682a92108259
Tom <tom.maxwell@cambridgeaudio.com>
2018-03-23 12:01:03 +0000

    add flow to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,77 @@
+// @flow
 import React from 'react';
+import type {Node} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../../redux/actions/login-actions';
 import styles from './Header.module.css';
 
-const Header = ({status, logout}) => {
+type StatusType = {
+  isLoggedIn: boolean,
+  email: string
+};
+
+const Header = ({
+  status,
+  logout,
+}: {status: StatusType, logout: () => void}): Node => {
   return (
     <div className={styles.header}>
       <Link className={styles.logo} to="/">Cambridge Audio | Learn</Link>
       <Navigation isLoggedIn={status.isLoggedIn} />
-      <User isLoggedIn={status.isLoggedIn} email={status.email} logout={logout} />
+      <User
+        isLoggedIn={status.isLoggedIn}
+        email={status.email}
+        logout={logout} />
     </div>
   );
 };
 
-const Navigation = ({isLoggedIn}) => (
+const Navigation = ({isLoggedIn}: {isLoggedIn: boolean}): Node => (
   isLoggedIn
     ? <Link className={styles.link} to="/courses">Courses</Link>
     : null
 );
 
-const User = ({isLoggedIn, email, logout}) => (
+const User = ({
+  isLoggedIn,
+  email,
+  logout,
+}: {isLoggedIn: boolean, email: string, logout: () => void}): Node => (
   <div className={styles.user}>
     {
       isLoggedIn
         ? <LoggedIn email={email} logout={logout} />
-      : <LoggedOut />
+        : <LoggedOut />
     }
   </div>
 );
 
-const LoggedOut = () => (
+const LoggedOut = (): Node => (
   <div>
     <Link className={styles.link} to="/register">Register</Link>
     <Link className={styles.link} to="/login">Login</Link>
   </div>
 );
 
-const LoggedIn = ({logout, email}) => (
+const LoggedIn = ({
+  logout,
+  email,
+}: {logout: () => void, email: string}): Node => (
   <div className={styles.loggedIn}>
     <div>{email}</div>
-    <button className={styles.logout} onClick={() => logout()}>
+    <button className={styles.logout} onClick={(): void => logout()}>
       Sign out
     </button>
   </div>
 );
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: any): any => ({
   status: state.status,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logout()),
+const mapDispatchToProps = (dispatch: any): any => ({
+  logout: (): void => dispatch(logout()),
 });
 
 const HeaderContainer = connect(
