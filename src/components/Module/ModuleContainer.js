9d36043946ff6e09f5960deb1f4caa3ed148b688
Tom <tom.maxwell@cambridgeaudio.com>
2018-03-29 13:34:13 +0100

    remove unused gear

--- src/components/Module/ModuleContainer.js
+++ src/components/Module/ModuleContainer.js
@@ -2,10 +2,7 @@ import {connect} from 'react-redux';
 import {ModuleLoader} from './Module';
 import {moduleDone} from '../../redux/actions/action-creators.js';
 
-const getModuleProgression = (moduleProgression) => moduleProgression;
-
 const mapStateToProps = (state) => ({
-  moduleStatuses: getModuleProgression(state.moduleProgression),
   resources: state.resources.data,
   loaded: state.resources.status === 'loaded' ? true : false,
 });
